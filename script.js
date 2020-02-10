a47de636900e22c319380df91db84bb6e142617f
David Ghazi <davidaghazi@gmail.com>
2020-02-10 12:47:47 -0500

    script.js update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Provided in assignment
+var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+var charactersLength = characters.length;
+
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener('click', writePassword)
+generateBtn.addEventListener('click', writePassword);
 
 function writePassword() {
   var password = criteriaPassword();
@@ -69,77 +72,18 @@ function criteriaPassword() {
 
   var generatedPassword = "";
 
-  // for (let i = 0; i < (parseInt(length) - minimumCount); i++) {
-  //   var randomNumberPicked = Math.floor(Math.random() * 4);
+  for (let i = 0; i < (parseInt(length) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
 
-  //   randomPasswordGenerated += randomNumberPicked;
-
-  // }
-
-  generatedPassword += minimumNumeric;
-  generatedPassword += minimumLowercase;
-  generatedPassword += minimumUppercase;
-  generatedPassword += minimumSpecialCharacters;
-
-  return generatedPassword;
+    generatedPassword += randomNumberPicked;
 
 }
+    
+    generatedPassword += minimumNumeric;
+    generatedPassword += minimumLowercase;
+    generatedPassword += minimumUppercase;
+    generatedPassword += minimumSpecialCharacters;
 
+    return generatedPassword;
 
-// console.log(getRandomLower());
-// console.log(getRandomUpper());
-// console.log(getRandomNumber());
-// console.log(getRandomSymbol());
-
-// function getRandomLower(){
-//   const lower = 'abcdefghijklmnopqrstuvwxyz';
-//   return symbols [Math.floor(Math.random()*)] 
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-// var passwordText = document.querySelector("#password");
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var length = prompt("How many characters will your password be? Enter a number between 8 and 128");
-//   var lowercase = confirm ("Would you like to include lowercase letters?");
-//   var uppercase = confirm ("Would you like to include uppercase letters?");
-//   var numeric = confirm ("Would you like to include numeric values?");
-//   var special = confirm ("Would you like to include special characters?");
-
-
-
-
-
-//   // var password = generatePassword();
-
-
-//   // passwordText.value = password;
-
-//   if (lowercase === true) function getRandomLower(){
-//     return String.fromCharCode(Math.floor(Math.random() * 26) +97)
-//   }
-// }
+}
